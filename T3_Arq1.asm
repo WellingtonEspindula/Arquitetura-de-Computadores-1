58b5e9f8ad979591223995945fce6ecb935a44fa
Wellington Espindula <wellington.m.espindula@gmail.com>
2019-06-21 00:00:56 -0300

    It's reading from keyboard. Started file_open implementation

--- T3_Arq1.asm
+++ T3_Arq1.asm
@@ -18,6 +18,8 @@
 CR       EQU    0DH ; codigo ASCII do caractere "carriage return"
 LF       EQU    0AH ; codigo ASCII do caractere "line feed"
 ATR_TELA EQU    0BH ; fundo preto, caractere ciano claro
+BKSPC     EQU    08H ; caractere ASCII "Backspace"
+ESCP      EQU    27  ; caractere ASCII "Escape" (tecla ESC)
 
 
 ; definicao do segmento de dados do programa
@@ -25,7 +27,8 @@ dados    segment
 msg_ini     db     '>>> Leitor de arquivo com velocidade variavel <<<', CR, LF
 ident       db     '>>> Autor: Wellington Espindula #00302367 <<<',CR,LF,LF,'$'
 msg_arq     db     'Digite o nome do arquivo: $'
-arquivo     dd     ?
+arquivo     dw     ?
+            db     CR, LF, '$'
 dados    ends
 
 ; definicao do segmento de pilha do programa
@@ -49,7 +52,12 @@ programa:
         call    write          
         lea     dx, msg_arq
         call    write
-        call    espera_tecla
+        lea     di, arquivo
+        call    gets
+        lea     dx, arquivo
+        call    write
+
+        
 
         
 
@@ -95,6 +103,103 @@ write   proc
          int    21h                ; chamada do DOS
          ret
 write   endp
+
+; Subrotina que recebe string do teclado
+; Recebe o endereco onde a string sera armazenada no registrador SI
+gets   proc
+end_str     dw      ?
+
+         mov    end_str, di         ; copia o endereco da string
+entrada: 
+         mov    ah,1
+         int    21h                ; le um caractere com eco
+
+         cmp    al,ESCP            ; compara com ESCAPE (tecla ESC)
+         jne     valida_enter
+         jmp    terminar 
+         
+valida_enter:
+         cmp    al,CR              ; compara com carriage return (tecla ENTER)
+         je     continua
+
+valida_bksp:
+         cmp    al,BKSPC           ; compara com 'backspace'
+         je     backspace
+
+         mov    [di],al            ; coloca caractere lido no buffer
+         inc    di
+         jmp    entrada
+
+backspace:
+         cmp    di,end_str
+         jne    adiante
+         mov    dl,' '              ; avanca cursor na tela
+         mov    ah,2
+         int    21h
+         jmp    entrada
+adiante:
+         mov    dl,' '              ; apaga ultimo caractere digitado
+         mov    ah,2
+         int    21h
+         mov    dl,BKSPC            ; recua cusor na tela
+         mov    ah,2
+         int    21h
+         dec    di
+         jmp    entrada
+
+continua: 
+         mov    byte ptr [di],0     ; forma string ASCIIZ com o nome do arquivo
+         mov    dl,LF               ; escreve LF na tela
+         mov    ah,2
+         int    21h
+         jmp    retorna
+
+terminar:
+         mov    ax,4c00h            ; funcao retornar ao DOS no AH
+                                    ; codigo de retorno 0 no AL
+         int    21h                 ; chamada do DOS
+
+retorna:
+         ret
+gets endp
+
+; TODO
+file_open   proc
+; abre arquivo para leitura 
+         mov    ah,3dh
+         mov    al,0
+         lea    dx,nome
+         int 21h
+         jnc    abriu_ok
+         lea    dx,erro             ; endereco da mensagem em DX
+         mov    ah,9                ; funcao exibir mensagem no AH
+         int    21h                 ; chamada do DOS
+         jmp    de_novo
+;
+abriu_ok: mov handler,ax
+laco:    mov ah,3fh                 ; l� um caractere do arquivo
+         mov bx,handler
+         mov cx,1
+         lea dx,buffer
+         int 21h
+         cmp ax,cx
+         jne fim
+         mov dl, buffer             ; escreve caractere na tela
+         mov ah,2
+         int 21h
+;         
+         mov dl, buffer             ; escreve na tela at� encontrar um LF (fim de linha)
+         cmp dl, LF
+         jne laco
+;   
+         mov ah,8                   ; espera pela digitacao de uma tecla qualquer
+         int 21h
+         jmp laco
+;
+fim:     mov ah,3eh                 ; fecha arquivo
+         mov bx,handler
+         int 21h
+file_open   endp
 
 espera_tecla proc
          mov    ah,0               ; funcao esperar tecla no AH
